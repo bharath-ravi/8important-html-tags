0851d1fc61375835ce925918f5a8f0ae749608c4
bharath-ravi <74550907+bharath-ravi@users.noreply.github.com>
2023-07-17 23:01:43 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 console.log('Hello!');
 const res='3'+2*2;
 console.log(res)
+function showContent() {
+  let temp = document.getElementsByTagName("template")[0];
+  let clon = temp.content.cloneNode(true);
+  document.body.appendChild(clon);
+}
